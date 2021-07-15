d7d06ec3f55472d9afa595babc52ca1e3cb23cf1
Robbie Pitts <me@robbie-pitts.com>
2021-07-15 00:01:52 -0400

    Make cli nicer

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,34 @@ mod files;
 use std::{str};
 use markdown_to_html::markdown_to_html;
 use files::find_files;
+use structopt::StructOpt;
 
 const SRC_DIR: &str = "src";
 const SRC_EXT: &str = "md";
 const DIST_DIR: &str = "dist";
 
+#[derive(StructOpt)]
+#[structopt(name = "distillate", about = "Markdown to HTML static site generator")]
+struct Opt {
+    #[structopt(subcommand)]
+    cmd: Option<Command>
+}
+
+#[derive(StructOpt)]
+enum Command {
+    Build
+}
+
 fn main() {
-    match markdown_to_html(SRC_DIR, SRC_EXT, DIST_DIR) {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
-    };
+    let args = Opt::from_args();
+
+    match args.cmd {
+        Some(Command::Build) => {
+            match markdown_to_html(SRC_DIR, SRC_EXT, DIST_DIR) {
+                Ok(_) => (),
+                Err(e) => println!("{}", e),
+            };
+        },
+        _ => Opt::clap().print_help().unwrap(),
+    }
 }
